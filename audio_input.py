3ad0a872e07aac49314b4085045d14cfede929b1
davey-meredith12 <143116633+davey-meredith12@users.noreply.github.com>
2024-10-21 13:29:21 -0600

    Using fft to get frequencies out into a list.
    Uses a thread and queue to process data using fft so the stream can continue recording

--- audio_input.py
+++ audio_input.py
@@ -1,30 +1,82 @@
 import pyaudio
 import wave
 import time
+import numpy
+import scipy
+import threading
+import queue
 
 FORMAT = pyaudio.paInt16
 CHANNELS = 1
 RATE = 44100
 CHUNK = 1024
 OUTPUT_FILENAME = "recorded_audio.wav"
+TIME_TO_RECORD = 3
 
 #instantiate
 audio = pyaudio.PyAudio()
 #set up stream
-stream = audio.open(format = FORMAT, channels = CHANNELS, rate = RATE, input = True, frames_per_buffer = CHUNK)
+stream = audio.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK)
 #read incoming audio
 frames = []
 
 start_time = time.time()
+fft_queue = queue.Queue()
+finished_recording = False
+output_frequencies = []
 
+#A function that will run on a thread that utilizes a queue to process data using fft.
+#All the frequencies will be put into the output_frequencies array
+def fft_thread():
+    while not finished_recording or fft_queue.qsize() > 0:
+        if fft_queue.qsize() > 0:
+            N = CHUNK
+            R = RATE
+
+            queue_data = fft_queue.get()
+            queue_data = numpy.frombuffer(queue_data, dtype=numpy.int16)
+
+            fft_result = scipy.fft.fft(queue_data)
+            # absolute value result to avoid imaginary numbers
+            fft_result = numpy.abs(fft_result)
+            #normalize result
+            fft_max = numpy.max(fft_result)
+            normalized_fft_result = fft_result / fft_max
+
+            relative_maximums, _ = scipy.signal.find_peaks(normalized_fft_result, height=.9)
+            # inverse of sample rate
+            d = 1 / R
+            frequencies = scipy.fft.fftfreq(N, d)
+            maximum_frequencies = frequencies[relative_maximums]
+            positive_maximum_frequencies = maximum_frequencies[maximum_frequencies > 0]
+            #add output frequencies to the list
+            output_frequencies.append(positive_maximum_frequencies)
+
+
+#start thread to begin processing data
+fft_thread = threading.Thread(target=fft_thread, args=[])
+fft_thread.start()
+
+#start recording
 while True:
     try:
+        #perform fft here
         data = stream.read(CHUNK)
         frames.append(data)
-    except:
+        fft_queue.put(data)
+
+    except Exception as e:
+        print("Exception Occured: ", e)
         break
-    if time.time() - start_time >= 5:
+
+    if time.time() - start_time >= TIME_TO_RECORD:
         break
+
+finished_recording = True
+fft_thread.join()
+
+print(output_frequencies)
+print(len(output_frequencies))
 
 #close up the stream
 stream.stop_stream()
