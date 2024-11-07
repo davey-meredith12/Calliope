dd15644f9d75be91df014b9f7a6b1b8b8a701a31
davey-meredith12 <143116633+davey-meredith12@users.noreply.github.com>
2024-11-07 14:37:26 -0700

    Created a note to frequency array converter
    Takes an array of array of string notes that correspond currently to quarter notes. Converts the string notes into frequencies that can be used for comparing

--- calliope/audio_grading.py
+++ calliope/audio_grading.py
@@ -1,19 +1,24 @@
 import time
 import numpy as np
+from frequency_note_conversion import note_to_freq
 
 #currently written for quarter notes
 simple_tune = [["A4"], ["R"], ["A4"], ["R"]]
 
-correct_notes = [[440, 444] ,[0],[440],[0]]
+correct_notes = [[440] ,[0],[440],[0]]
 BPM = 60
 FREQ_DIFF_ALLOWED = 1
 #correct notes and correct timing length will be the same
 
+#tune builder, takes notes and gives back frequencies to use in compare
 def notes_arr_to_freq_arr(notes):
-    freq_arr = []
-
-
-
+    to_return = []
+    for note_arr in notes:
+        freq = []
+        for note in note_arr:
+            freq.append(note_to_freq(note))
+        to_return.append(freq)
+    return to_return
 
 #compared one at a time against the correct_notes
 #Must be called timed based on the beat
@@ -25,10 +30,18 @@ def compare(input: list[float], time_since_start: float):
     return np.isclose(input, correct_notes[arr_loc], atol = FREQ_DIFF_ALLOWED)
 
 if __name__ == '__main__':
-    user_input = [[440.3, 444], [0], [441], [0]]
-    for i in range(4):
-        timeToSleep = 60/BPM
-        timing = i * timeToSleep
-        print(compare(user_input[i], timing))
-        print(timing)
-        time.sleep(timeToSleep)
+    print("running")
+    print(simple_tune)
+    tune_freq = notes_arr_to_freq_arr(simple_tune)
+    print(tune_freq)
+
+
+
+
+    # user_input = [[440.3, 444], [0], [441], [0]]
+    # for i in range(4):
+    #     timeToSleep = 60/BPM
+    #     timing = i * timeToSleep
+    #     print(compare(user_input[i], timing))
+    #     print(timing)
+    #     time.sleep(timeToSleep)
