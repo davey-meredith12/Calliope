ce114dd49b797c2b0f8351ca3a1800d82835d625
davey-meredith12 <143116633+davey-meredith12@users.noreply.github.com>
2024-11-14 14:41:25 -0700

    Updated grading system and made into a class
    Need to do more detailed tests later and write tests

--- calliope/__main__.py
+++ calliope/__main__.py
@@ -1,7 +1,21 @@
+import time
+
 from audio_input import AudioInput
 from frequency_note_conversion import freq_to_note
+from audio_grading import AudioGrading
+
+
+tune_arr = [["C4"], ["R"], ["C4"], ["R"], ["C4"], ["C4"], ["C4"], ["C4"]]
+bpm = 60
+
+audio_grading = AudioGrading(tune_arr, bpm)
+audio_grader = audio_grading.grade()
+next(audio_grader)
 
 for frequencies in AudioInput().start():
+    print(audio_grader.send(frequencies))
     for frequency in frequencies:
-        print(freq_to_note(frequency))
+        print(freq_to_note(frequency), end=" ")
     print()
+
+print("done")
